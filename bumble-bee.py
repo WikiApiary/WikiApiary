ad0cc21f6a099b5c917b337761e5b970b1c6d962
Jamie Thingelstad <jamie@thingelstad.com>
2013-01-06 09:55:02 -0600

    Changes to make PyLint (more) happy. Convert indents to spaces.

--- bumble-bee.py
+++ bumble-bee.py
@@ -12,79 +12,83 @@ import urllib2
 from urllib2 import Request, urlopen, URLError, HTTPError
 from simplemediawiki import MediaWiki
 
-def get_config(config_file = 'bumble-bee.cfg'):
-	try:
-		config = ConfigParser.ConfigParser()
-		config.read(config_file)
-	except IOError:
-		print "Cannot open %s." % ( config_file )
 
-	return config
+def get_config(config_file='bumble-bee.cfg'):
+    try:
+        config = ConfigParser.ConfigParser()
+        config.read(config_file)
+    except IOError:
+        print "Cannot open %s." % config_file
+
+    return config
+
 
 def get_args():
-	parser = argparse.ArgumentParser(prog="Bumble Bee",
-						description="retrieves usage and statistic information for WikiApiary")
-	parser.add_argument("-s", "--segment",
-				help="only work on websites in defined segment")
-	parser.add_argument("-d", "--debug", action="store_true",
-				help="do not write any changes to wiki or database")
-	parser.add_argument("--config", default="bumble-bee.cfg",
-				help="use an alternative config file")
-	parser.add_argument("-v", "--verbose", action="count", default=0,
-				help="increase output verbosity")
-	parser.add_argument("--version", action="version", version="%(prog)s 0.1")
+    parser = argparse.ArgumentParser(prog="Bumble Bee",
+                        description="retrieves usage and statistic information for WikiApiary")
+    parser.add_argument("-s", "--segment",
+                help="only work on websites in defined segment")
+    parser.add_argument("-d", "--debug", action="store_true",
+                help="do not write any changes to wiki or database")
+    parser.add_argument("--config", default="bumble-bee.cfg",
+                help="use an alternative config file")
+    parser.add_argument("-v", "--verbose", action="count", default=0,
+                help="increase output verbosity")
+    parser.add_argument("--version", action="version", version="%(prog)s 0.1")
 
-	# All set, now get the arguments
-	args = parser.parse_args()
+    # All set, now get the arguments
+    args = parser.parse_args()
 
-	return args
+    return args
+
 
 def get_websites(wiki, segment):
-	global config, args
+    global config
+    global args
 
-	if segment is None:
-		if args.verbose >= 1:
-			print "Retrieving all websites of all segments."
-		sites = wiki.call({'action': 'ask', 'query': '[[Category:Website]][[Is validated::True]][[Is active::True]][[Collect statistics::+]][[Collect semantic statistics::+]]|?Has API URL|?Check every|?Creation date|?Has ID|?Collect statistics|?Collect semantic statistics|sort=Creation date|order=asc|limit=500 '})
-	else:
-		if args.verbose >= 1:
-			print "Only retrieving segment %s." % args.segment
-		sites = wiki.call({'action': 'ask', 'query': '[[Category:Website]][[Is validated::True]][[Is active::True]][[Collect statistics::+]][[Collect semantic statistics::+]][[Has bot segment::' + segment + ']]|?Has API URL|?Check every|?Creation date|?Has ID|?Collect statistics|?Collect semantic statistics|sort=Creation date|order=asc|limit=500 '})
+    if segment is None:
+        if args.verbose >= 1:
+            print "Retrieving all websites of all segments."
+        sites = wiki.call({'action': 'ask', 'query': '[[Category:Website]][[Is validated::True]][[Is active::True]][[Collect statistics::+]][[Collect semantic statistics::+]]|?Has API URL|?Check every|?Creation date|?Has ID|?Collect statistics|?Collect semantic statistics|sort=Creation date|order=asc|limit=500 '})
+    else:
+        if args.verbose >= 1:
+            print "Only retrieving segment %s." % args.segment
+        sites = wiki.call({'action': 'ask', 'query': '[[Category:Website]][[Is validated::True]][[Is active::True]][[Collect statistics::+]][[Collect semantic statistics::+]][[Has bot segment::' + segment + ']]|?Has API URL|?Check every|?Creation date|?Has ID|?Collect statistics|?Collect semantic statistics|sort=Creation date|order=asc|limit=500 '})
 
-	if len(sites['query']['results']) > 0:
-		for pagename, site in sites['query']['results'].items():
-			if args.verbose >= 2:
-				print "Adding %s." % pagename.encode('utf8')
-	else:
-		raise Exception("No sites were returned to work on.")
+    if len(sites['query']['results']) > 0:
+        for pagename, site in sites['query']['results'].items():
+            if args.verbose >= 2:
+                print "Adding %s." % pagename.encode('utf8')
+    else:
+        raise Exception("No sites were returned to work on.")
 
 
 def main():
-	# Set global socket timeout for all our web requests
-	socket.setdefaulttimeout(5)
+    # Set global socket timeout for all our web requests
+    socket.setdefaulttimeout(5)
 
-	# Get command line options
-	global args
-	args = get_args()
+    # Get command line options
+    global args
+    args = get_args()
 
-	# Get configuration settings
-	global config
-	config = get_config(args.config)
+    # Get configuration settings
+    global config
+    config = get_config(args.config)
 
-	# Setup our database connection and get a cursor to work with
-	apiarydb = mdb.connect( host=config.get('ApiaryDB', 'hostname'),
-				db=config.get('ApiaryDB', 'database'),
-				user=config.get('ApiaryDB', 'username'),
-				passwd=config.get('ApiaryDB', 'password') )
+    # Setup our database connection and get a cursor to work with
+    apiarydb = mdb.connect(
+        host=config.get('ApiaryDB', 'hostname'),
+        db=config.get('ApiaryDB', 'database'),
+        user=config.get('ApiaryDB', 'username'),
+        passwd=config.get('ApiaryDB', 'password'))
 
-	# Setup our connection to the wiki too
-	wiki = MediaWiki( config.get('WikiApiary', 'API') )
-	wiki.login( config.get('WikiApiary', 'Username'), config.get('WikiApiary', 'Password') )
+    # Setup our connection to the wiki too
+    wiki = MediaWiki(config.get('WikiApiary', 'API'))
+    wiki.login(config.get('WikiApiary', 'Username'), config.get('WikiApiary', 'Password'))
 
-	sites = get_websites( wiki, args.segment )
+    sites = get_websites(wiki, args.segment)
 
 
 # Run main
-if __name__ == '__main__': 
-	main() 
-
+if __name__ == '__main__':
+    main()
