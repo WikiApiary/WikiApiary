0a688875701b3aeb4a9cc642cbe77bcea5f71ed7
Jamie Thingelstad <jamie@thingelstad.com>
2013-01-06 21:03:56 -0600

    Working with the website_status table and pulling json data.
    Properly pulling json data via funciton. Setting and updating website_status table.

--- bumble-bee.py
+++ bumble-bee.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import datetime
+import pytz
 import ConfigParser
 import argparse
 import socket
@@ -42,6 +43,36 @@ def get_args():
     return args
 
 
+def sqlutcnow():
+    now = datetime.datetime.utcnow()
+    now = now.replace(tzinfo=pytz.utc)
+    now = now.replace(microsecond=0)
+    return now.strftime('%Y-%m-%d %H:%M:%S')
+
+def pull_json(data_url):
+    # Get JSON data via API and return the JSON structure parsed
+    global args
+    global config
+
+    req = urllib2.Request(data_url)
+    req.add_header('User-Agent', config.get('Bumble Bee', 'User-Agent'))
+    opener = urllib2.build_opener()
+
+    try:
+        t1 = datetime.datetime.now()
+        f = opener.open(req)
+        duration = datetime.datetime.now() - t1
+    except socket.timeout:
+        print "Socket timeout!"
+    except HTTPError as e:
+        print "Error code ", e.code
+    except URLError as e:
+        print "Reason: ", e.reason
+    else:
+        # It all worked!
+        data = simplejson.load(f)
+        return data, duration
+
 def get_websites(wiki, segment):
     global config
     global args
@@ -72,15 +103,107 @@ def get_websites(wiki, segment):
                 'pagename':pagename.encode('utf8'),
                 'Has API URL':site['printouts']['Has API URL'][0],
                 'fullurl':site['fullurl'].encode('utf8'),
-                'Check every':site['printouts']['Check every'][0],
+                'Check every':int(site['printouts']['Check every'][0]),
                 'Collect statistics':(site['printouts']['Collect statistics'][0]=="t"), # This is a boolean but it's returned as t or f, let's make it a boolean again
-                'Has ID':site['printouts']['Has ID'][0],
+                'Has ID':int(site['printouts']['Has ID'][0]),
                 'Collect semantic statistics':(site['printouts']['Collect semantic statistics'][0]=="t") # This is a boolean but it's returned as t or f, let's make it a boolean again
                 })
 
         return my_sites
     else:
         raise Exception("No sites were returned to work on.")
+
+
+def get_status(db, site):
+    """
+    get_status will query the website_status table in ApiaryDB. It makes the decision if new
+    data should be retrieved for a given website. Two booleans are returned, the first to
+    tell if new statistics information should be requested and the second to pull general information.
+    """
+    global args
+    # Get the timestamps for the last statistics and general pulls
+    cur = db.cursor()
+    temp_sql = "SELECT last_statistics, last_general, check_every_limit FROM website_status WHERE website_id = %d" % site['Has ID']
+    cur.execute(temp_sql)
+    rows_returned = cur.rowcount
+
+    if rows_returned == 1:
+        # Let's see if it's time to pull information again
+        data = cur.fetchone()
+        cur.close()
+
+        (last_statistics, last_general, check_every_limit) = data[0:3]
+        if args.verbose >= 3:
+            print "last_stats: %s" % last_statistics
+            print "last_general: %s" % last_general
+            print "check_every_limit: %s" % check_every_limit
+
+        #TODO: make this check the times!
+        return (True, True)
+
+    elif rows_returned == 0:
+        cur.close()
+        # This website doesn't have a status, so we should check everything
+        if args.verbose >= 3:
+            print "website has never been checked before"
+        return (True, True)
+
+    else:
+        raise Exception("Status check returned multiple rows.")
+
+
+def update_status(db, site):
+    global args
+    cur = db.cursor()
+
+    # Update the website_status table
+    my_now = sqlutcnow()
+    temp_sql = "UPDATE website_status SET last_statistics = '%s' WHERE website_id = %d" % (my_now, site['Has ID'])
+    if args.verbose >= 3:
+        print "SQL: %s" % temp_sql
+    cur.execute(temp_sql)
+    rows_affected = cur.rowcount
+    if rows_affected == 0:
+        # No rows were updated, this website likely didn't exist before, so we need to insert the first time
+        if args.verbose >= 2:
+            print "No website_status record exists for ID %d, creating one" % site['Has ID']
+        temp_sql = "INSERT website_status (website_id, last_statistics, last_general, check_every_limit) "
+        temp_sql += "VALUES (%d, \"%s\", \"%s\", %d)" % (site['Has ID'], my_now, my_now, 240)
+        if args.verbose >= 3:
+            print "SQL: %s" % temp_sql
+        cur.execute(temp_sql)
+
+    cur.close()
+    db.commit()
+
+
+
+def record_statistics(db, site):
+
+    # Go out and get the statistic information
+    data_url = site['Has API URL'] + "?action=query&meta=siteinfo&siprop=statistics&format=json"
+    (data, duration) = pull_json(data_url)
+
+    # Record the new data into the DB
+    print "Duration: %s" % duration
+    print data
+
+    # Update the status table that we did our work!
+    update_status(db, site)
+
+def record_smwinfo(db, site):
+    print "In get_smwinfo"
+    print site
+
+
+def record_general(wiki, site):
+    print "In get_general"
+    print site
+
+
+def record_extensions(wiki, site):
+    print "In get_extensions"
+    print site
 
 
 def main():
@@ -108,9 +231,17 @@ def main():
 
     sites = get_websites(wiki, args.segment)
 
-    if args.verbose >= 3:
-        for i in sites:
-            print i
+    for site in sites[0:2]:
+        req_statistics = False
+        req_general = False
+        (req_statistics, req_general) = get_status(apiarydb, site)
+        if req_statistics:
+            record_statistics(apiarydb, site)
+            record_smwinfo(apiarydb, site)
+        if req_general:
+            record_general(wiki, site)
+            record_extensions(wiki, site)
+
 
 # Run main
 if __name__ == '__main__':
