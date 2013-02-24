186aa6eff4c253cc6697f9bb9579a7657831f323
Jamie Thingelstad <jamie@thingelstad.com>
2013-02-23 19:48:13 -0600

    Merge completion stats into one log item.

--- bumble-bee/bumble-bee.py
+++ bumble-bee/bumble-bee.py
@@ -381,11 +381,10 @@ class BumbleBee(ApiaryBot):
         else:
             message = "Completed processing for all websites."
         message += " Processed %d websites." % i
-        self.botlog(bot='Bumble Bee', duration=float(duration), message=message)
-        message = "Stats: statistics %d smwinfo %d general %d extensions %d skins %d skipped_stats: %d skipped_general: %d" % (
+        message += " Stats: statistics %d smwinfo %d general %d extensions %d skins %d skipped_stats: %d skipped_general: %d" % (
             self.stats['statistics'], self.stats['smwinfo'], self.stats['general'],
             self.stats['extensions'], self.stats['skins'], self.stats['skippedstatistics'], self.stats['skippedgeneral'])
-        self.botlog(bot='Bumble Bee', message=message)
+        self.botlog(bot='Bumble Bee', duration=float(duration), message=message)
 
 
 # Run
