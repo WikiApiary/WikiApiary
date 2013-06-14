d138ba0f5a16d0dd072dfd4a684e89b49e084bbc
Jamie Thingelstad <jamie@thingelstad.com>
2013-06-13 21:23:09 -0500

    TropicalBot to sync TropicalWikis farm to WikiApiary.

---
+++ maint/tropicalwikis-farmer.py
@@ -0,0 +1,149 @@
+#!/usr/bin/python
+"""
+Pull configuration files from WikiMedia to update WikiApiary.
+
+"""
+
+import requests
+from BeautifulSoup import BeautifulSoup
+import ConfigParser
+import time
+from simplemediawiki import MediaWiki
+
+
+class TropicalWikis:
+
+    # Array to append sites to
+    sites = []
+    # This file is a list of all the database names used by wikimedia
+    # we can use this to try and derive the names of various wikis
+    source_list = 'http://www.tropicalwikis.com/wiki/Special:Farmer/list'
+    # Blank reference to store mediawiki object in
+    wikiapiary = {}
+    # Edit token
+    my_token = ""
+    # Counter
+    create_counter = 0
+
+    def __init__(self):
+        config = ConfigParser.ConfigParser()
+        config.read('../apiary.cfg')
+
+        self.wikiapiary = MediaWiki(config.get('WikiApiary', 'api'))
+        self.wikiapiary.login(config.get('TropicalBot', 'Username'), config.get('TropicalBot', 'Password'))
+
+        # We need an edit token on wiki2
+        c = self.wikiapiary.call({
+            'action': 'query',
+            'titles': 'Foo',
+            'prop': 'info',
+            'intoken': 'edit'
+        })
+        self.my_token = c['query']['pages']['-1']['edittoken']
+
+    def getList(self):
+        soup = BeautifulSoup(requests.get(self.source_list).text)
+        i = 1
+        for item in soup.findAll("a", {"class": "external text"}):
+            site = (item.contents[0], item["href"])
+            print i, site
+            self.sites.append(site)
+            i += 1
+
+    def createSite(self, name, url):
+        siteTemplate = """{{Website
+|Name=%s
+|URL=%s
+|Image=Default website image.png
+|Farm=TropicalWikis
+|Collection method=API
+|API URL=%s
+|Collect general data=Yes
+|Collect extension data=Yes
+|Collect skin data=Yes
+|Collect statistics=Yes
+|Collect semantic statistics=No
+|Collect semantic usage=No
+|Collect logs=No
+|Collect recent changes=No
+|Statistics URL=
+|Collect statistics stats=Yes
+|Check every=240
+|Audited=No
+|Validated=No
+|Curated=No
+|Active=No
+|Demote=No
+|Defunct=No
+|Error=No
+|Featured website vote=0
+}}
+"""
+        api_url = "%s/w/api.php" % url
+
+        # Make sure a page doesn't exist with this name already
+        c = self.wikiapiary.call({
+            'action': 'query',
+            'titles': name
+        })
+        try:
+            if c['query']['pages']['-1']:
+                print "No duplicate name detected."
+        except:
+            # Duplicate detected
+            name = "%s (TropicalWikis)" % name
+
+        my_template = siteTemplate % (name, url, api_url)
+        print my_template
+
+        c = self.wikiapiary.call({
+            'action': 'edit',
+            'title': name,
+            'text': my_template,
+            'token': self.my_token,
+            'bot': 'true',
+            'summary': 'Creating entry for %s' % name
+        })
+        print c
+        self.create_counter += 1
+
+    def checkSite(self, site):
+        print "Checking %s" % site[1]
+
+        # Construct Ask query for WikiApiary
+        my_query = ''.join([
+            "[[Has API URL::%s/w/api.php]]" % site[1]
+        ])
+        # Execute the query against WikiApiary
+        c = self.wikiapiary.call({
+            'action': 'ask',
+            'query': my_query
+        })
+
+        # Return the count of results for the query
+        return int(c['query']['meta']['count'])
+
+    def main(self):
+        # Get the list of tokens from the config file
+        self.getList()
+
+        for site in self.sites:
+            print "\nProcessing %s" % site[0]
+
+            # Use a guess of the API domain to see if we have it already
+            siteCount = self.checkSite(site)
+
+            if siteCount == 0:
+                print "%s is not in WikiApiary, validating stats." % site[0]
+                # Now add it to WikiApiary
+                self.createSite(site[0], site[1])
+                time.sleep(3)
+            elif siteCount == 1:
+                print "%s already exists, skipping." % site[0]
+            elif siteCount > 1:
+                print "ERROR: %s found %d websites, which should never happen." % (site[0], siteCount)
+
+# Run
+if __name__ == '__main__':
+    myClass = TropicalWikis()
+    myClass.main()
