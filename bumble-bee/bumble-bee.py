c1e80628bd8b6ec0fc01b6e6acf5b09d38d49387
Jamie Thingelstad <jamie@thingelstad.com>
2013-10-02 07:24:41 -0500

    Grab and set netblock organization information.

--- bumble-bee/bumble-bee.py
+++ bumble-bee/bumble-bee.py
@@ -32,6 +32,7 @@ import urlparse
 import pygeoip
 sys.path.append('../lib')
 from apiary import ApiaryBot
+from PyWhoisAPI import *
 
 
 class BumbleBee(ApiaryBot):
@@ -504,6 +505,16 @@ class BumbleBee(ApiaryBot):
         try:
             reverse_host = socket.gethostbyaddr(addr)[0]
             template_block += "|Reverse lookup=%s\n" % (self.ProcessMultiprops(site_id, 'reverse_host', reverse_host))
+        except:
+            pass
+
+        # Now lets get the netblock information
+        try:
+            whois = Whois()
+            netblock_owner = whois.getNetworkRegistrationRelatedToIP(addr, format='json')['net']['orgRef']['@name']
+            netblock_owner_handle = whois.getNetworkRegistrationRelatedToIP(addr, format='json')['net']['orgRef']['@handle']
+            template_block += "|Netblock organization=%s\n" % (netblock_owner)
+            template_block += "|Netblock organization handle=%s\n" % netblock_owner_handle
         except:
             pass
 
