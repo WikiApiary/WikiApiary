e56b678a462c334a4009e0de43860efcf9141e14
Jamie Thingelstad <jamie@thingelstad.com>
2013-11-30 07:12:29 -0600

    Restructured project directory.

---
+++ tests/test_website.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import unittest
+from ..apiary import website
+
+
+class test_website(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    
+    def test_website_id(self):
+        site = website.Website(18, 'WikiApiary', 'http://wikiapiary.com/w/api.url')
+        self.assertEqual(site.get_id(), 18, "Site ID is not correct.")
+
+
+if __name__ == '__main__':
+    unittest.main()
