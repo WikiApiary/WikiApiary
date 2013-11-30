bef3bdd7d1de47616ba230d342b1f85f809862a3
Jamie Thingelstad <jamie@thingelstad.com>
2013-11-30 08:09:14 -0600

    trailing space

--- tests/test_website.py
+++ tests/test_website.py
@@ -8,7 +8,7 @@ class test_website(unittest.TestCase):
 
     def setUp(self):
         pass
-    
+
     def test_website_id(self):
         site = website.Website(18, 'WikiApiary', 'http://wikiapiary.com/w/api.url')
         self.assertEqual(site.get_id(), 18, "Site ID is not correct.")
