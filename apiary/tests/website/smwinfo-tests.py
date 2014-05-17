2d41b56601d7ea7b58cac4a356f32a723193d933
Jamie Thingelstad <jamie@thingelstad.com>
2014-05-16 19:42:23 -0500

    Moved smwinfo into task class. Nothing left in tasks.py.

---
+++ apiary/tests/website/smwinfo-tests.py
@@ -0,0 +1,21 @@
+"""
+Exercise the Website class to insure the methods operate
+as expected.
+"""
+# pylint: disable=C0301,W0622
+
+import unittest
+if __name__ == "__main__" and __package__ is None:
+    __package__ = "WikiApiary.apiary.tests"
+from WikiApiary.apiary.tasks.website.smwinfo import GetSMWInfoTask
+
+
+class TestGetSMWInfoTask(unittest.TestCase):
+    """Run some tests."""
+
+    def test_smwinfo_task(self):
+        task = GetSMWInfoTask()
+        task.run(18, 'WikiApiary', 'https://wikiapiary.com/w/api.php')
+
+if __name__ == '__main__':
+    unittest.main()
