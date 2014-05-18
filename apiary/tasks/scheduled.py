65d346bb1729cd70455a12e9b0f9ff4067d8be69
Jamie Thingelstad <jamie@thingelstad.com>
2014-05-18 07:14:33 -0500

    Set celery beat scheduled tasks.
    - Created new scheduled module to put the tasks in.

---
+++ apiary/tasks/scheduled.py
@@ -0,0 +1,19 @@
+"""
+Module to kick off celery beat scheduled tasks.
+Celery cannot invoke object methods directly, so these tasks
+take care of that.
+"""
+
+from WikiApiary.apiary.tasks.bot.websitesegment import ProcessWebsiteSegment
+from WikiApiary.celery import app
+import logging
+
+
+LOGGER = logging.getLogger()
+
+@app.task(name='run_segment')
+def run_segment(segment_id):
+    """Invoke and run the website segment."""
+    LOGGER.info("Running segment %d" % segment_id)
+    # task = ProcessWebsiteSegment()
+    # task.run(segment_id)
--- celery.py
+++ celery.py
@@ -5,6 +5,9 @@ Base celery app definition.
 
 from __future__ import absolute_import
 from celery import Celery
+from celery.schedules import crontab
+from datetime import timedelta
+
 
 app = Celery(
     'WikiApiary',
@@ -20,14 +23,92 @@ app = Celery(
         'WikiApiary.apiary.tasks.website.skins',
         'WikiApiary.apiary.tasks.website.smwinfo',
         'WikiApiary.apiary.tasks.website.statistics',
-        'WikiApiary.apiary.tasks.website.whoislookup' 
+        'WikiApiary.apiary.tasks.website.whoislookup',
+        'WikiApiary.apiary.tasks.scheduled'
     ]
 )
 
 # Optional configuration, see the application user guide.
 app.conf.update(
     CELERY_TASK_RESULT_EXPIRES=3600,
-    CELERY_ACCEPT_CONTENT = ['pickle', 'json']
+    CELERY_ACCEPT_CONTENT = ['pickle', 'json'],
+    CELERYBEAT_SCHEDULE = {
+        'run_segment_0': {
+            'task': 'run_segment',
+            'schedule': crontab(minute='0,15,30,45'),
+            'args': [0]
+        },
+        'run_segment_1': {
+            'task': 'run_segment',
+            'schedule': crontab(minute='1,16,31,46'),
+            'args': [1]
+        },
+        'run_segment_2': {
+            'task': 'run_segment',
+            'schedule': crontab(minute='2,17,32,47'),
+            'args': [2]
+        },
+        'run_segment_3': {
+            'task': 'run_segment',
+            'schedule': crontab(minute='3,18,33,48'),
+            'args': [3]
+        },
+        'run_segment_4': {
+            'task': 'run_segment',
+            'schedule': crontab(minute='4,19,34,49'),
+            'args': [4]
+        },
+        'run_segment_5': {
+            'task': 'run_segment',
+            'schedule': crontab(minute='5,20,35,50'),
+            'args': [5]
+        },
+        'run_segment_6': {
+            'task': 'run_segment',
+            'schedule': crontab(minute='6,21,36,51'),
+            'args': [6]
+        },
+        'run_segment_7': {
+            'task': 'run_segment',
+            'schedule': crontab(minute='7,22,37,52'),
+            'args': [7]
+        },
+        'run_segment_8': {
+            'task': 'run_segment',
+            'schedule': crontab(minute='8,23,38,53'),
+            'args': [8]
+        },
+        'run_segment_9': {
+            'task': 'run_segment',
+            'schedule': crontab(minute='9,24,39,54'),
+            'args': [9]
+        },
+        'run_segment_10': {
+            'task': 'run_segment',
+            'schedule': crontab(minute='10,25,40,55'),
+            'args': [10]
+        },
+        'run_segment_11': {
+            'task': 'run_segment',
+            'schedule': crontab(minute='11,26,41,56'),
+            'args': [11]
+        },
+        'run_segment_12': {
+            'task': 'run_segment',
+            'schedule': crontab(minute='12,27,42,57'),
+            'args': [12]
+        },
+        'run_segment_13': {
+            'task': 'run_segment',
+            'schedule': crontab(minute='13,28,43,58'),
+            'args': [13]
+        },
+        'run_segment_14': {
+            'task': 'run_segment',
+            'schedule': crontab(minute='14,29,44,59'),
+            'args': [14]
+        }
+    }
 )
 
 if __name__ == '__main__':
